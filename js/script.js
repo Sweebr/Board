3ec60417b35037044ee4632910ecba910c091826
René <renevs@dualdev.com>
2013-04-15 18:04:25 +0200

    Fixed rotation icons shit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).foundation();
 
 (function(window, body, privates, undefined) {
-  var icons;
+  var icons, colors;
 
   // Attach events
   privates.attach = function attach() {
@@ -10,24 +10,27 @@ $(document).foundation();
 
   // Pick icons from the list
   icons = [ 'check-empty', 'refresh', 'group', 'check', 'ban-circle', 'time' ];
-  pickIcon = function pickIcon() {
+  colors = [ 'white', 'orange', 'blue', 'green', 'red', 'gray' ];
+  privates.pickIcon = function pickIcon() {
     var elem = $(this).find( 'span' ) 
-      , currentClass = elem.attr( 'class' );
+      , currentClass = elem.attr( 'class' ).substr(5);
     elem.removeAttr( 'class' );
 
     // Find current active icon
-    privates.pickClass(currentClass, icons, elem);
+    privates.pickClass(currentClass, icons, elem, function(elem, list, index){
+      elem.addClass( 'icon-' + list[( index )] ).parent().parent().parent().parent().removeAttr( 'class' ).addClass( colors[( index )] + ' row item' );
+    });
   };
 
   // Pick the next class in this list
-  pickClass = function pickClass(currentClass, list, elem) {
+  privates.pickClass = function pickClass(currentClass, list, elem, cb) {
     elem.removeAttr( 'class' );
     var currentIndex = list.indexOf( currentClass );
     if( (currentIndex+1) > (list.length-1) ) {
-      elem.addClass( list[0] );
+      cb(elem, list, 0);
     }
     else {
-      elem.addClass( list[(currentIndex+1)] );
+      cb(elem, list, (currentIndex+1));
     };
   };
 
