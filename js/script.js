a1cda13fb6450619d68f28f8faa06164f5b4dd0d
René <renevs@dualdev.com>
2013-04-15 18:36:27 +0200

    Add dynamic items

--- js/script.js
+++ js/script.js
@@ -3,6 +3,47 @@ $(document).foundation();
 (function(window, body, privates, undefined) {
   var icons, colors, assignees;
 
+  // Get unique number for this row
+  privates.generateNumber = function() {
+    return 'task:' + Object.keys(localStorage).length;
+  };
+  privates.findNumber = function(elem) {
+    return elem.closest( '.row.item' ).attr( 'id' );
+  };
+  privates.updateItem = function(elem, type, item) {
+    var id = privates.findNumber(elem);
+
+    // Get localStorage item
+    var currentLocalItem = localStorage.getItem( id );
+    currentLocalItem[( type )] = item;
+    localStorage.setItem( id, currentLocalItem );
+  };
+
+  // Create a new row
+  privates.create = function() {
+    var example = body.find( '.row.item.clone' ).clone();
+    example.attr( 'id', privates.generateNumber() );
+    example.appendTo( 'body' );
+    return example;
+  };
+
+  // Read localStorage
+  privates.read = function() {
+    Object.keys(localStorage).forEach(function(keyName){
+      var item = localStorage.getItem( keyName )
+        , elem = privates.create();
+
+      // Update inputs
+      elem.find( '.name.columns input' ).val( item.text );
+      elem.find( '.date.columns input' ).val( item.date );
+
+      // Update icon
+      privates.pickIcon.apply(elem.find( '.icon.columns' ), [item.icon]);
+      privates.pickAvatar.apply(elem.find( '.icon.columns' ), [item.avatar]);
+    });
+    privates.attach();
+  };
+
   // Attach events
   privates.attach = function attach() {
     body.on( 'click', '.icon.columns', privates.pickIcon );
@@ -11,27 +52,49 @@ $(document).foundation();
 
   // Pick avatar from the list
   assignees = [ 'chase', 'rene' ];
-  privates.pickAvatar = function pickAvatar() {
+  privates.pickAvatar = function pickAvatar(currentItem) {
     var elem = $(this).find( 'span' ) 
-      , currentClass = elem.attr( 'class' ).replace( 'avatar ', '' );
+      , currentClass = elem.attr( 'class' ).replace( 'avatar ', '' )
+      , picker = function(elem, list, index){
+          // Update localStorage
+          privates.updateItem( elem, 'avatar', list[( index )] );
+
+          // Update local class
+          elem.addClass( 'avatar ' + list[( index )] );
+        };
+
+    // Overwrite current class
+    if( 'string' == typeof currentItem ) {
+      elem.removeAttr( 'class' );
+      return picker(elem, assignees, assignees.indexOf(currentItem));
+    }
 
     // Find current active icon
-    privates.pickClass(currentClass, assignees, elem, function(elem, list, index){
-      elem.addClass( 'avatar ' + list[( index )] );
-    });
+    privates.pickClass(currentClass, assignees, elem, picker);
   };
 
   // Pick icons from the list
   icons = [ 'check-empty', 'refresh', 'group', 'check', 'ban-circle', 'time' ];
   colors = [ 'white', 'orange', 'blue', 'green', 'red', 'gray' ];
-  privates.pickIcon = function pickIcon() {
+  privates.pickIcon = function pickIcon(currentItem) {
     var elem = $(this).find( 'span' ) 
-      , currentClass = elem.attr( 'class' ).substr(5);
+      , currentClass = elem.attr( 'class' ).substr(5)
+      , picker = function(elem, list, index){
+          // Update localStorage
+          privates.updateItem( elem, 'icon', list[( index )] );
+
+          // Update local class
+          elem.addClass( 'icon-' + list[( index )] ).closest( '.row.item' ).removeAttr( 'class' ).addClass( colors[( index )] + ' row item' );
+        };
+
+    // Overwrite current class
+    if( 'string' == typeof currentItem ) {
+      elem.removeAttr( 'class' );
+      return picker(elem, icons, icons.indexOf(currentItem));
+    }
 
     // Find current active icon
-    privates.pickClass(currentClass, icons, elem, function(elem, list, index){
-      elem.addClass( 'icon-' + list[( index )] ).parent().parent().parent().parent().removeAttr( 'class' ).addClass( colors[( index )] + ' row item' );
-    });
+    privates.pickClass(currentClass, icons, elem, picker);
   };
 
   // Pick the next class in this list
@@ -47,4 +110,4 @@ $(document).foundation();
   };
 
   return privates;
-})(window, $( 'body' ), {}).attach();
+})(window, $( 'body' ), {}).read();
