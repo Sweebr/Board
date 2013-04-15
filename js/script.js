cf79f05fa97a1df2b26e2fd8dcd97d3cdf7d4017
René <renevs@dualdev.com>
2013-04-15 17:52:11 +0200

    Added icon picker?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 $(document).foundation();
+
+(function(window, body, privates, undefined) {
+  var icons;
+
+  // Attach events
+  privates.attach = function attach() {
+    body.on( 'click', '.icon.columns', privates.pickIcon );
+  };
+
+  // Pick icons from the list
+  icons = [ 'check-empty', 'refresh', 'group', 'check', 'ban-circle', 'time' ];
+  pickIcon = function pickIcon() {
+    var elem = $(this).find( 'span' ) 
+      , currentClass = elem.attr( 'class' );
+    elem.removeAttr( 'class' );
+
+    // Find current active icon
+    var currentIndex = icons.indexOf( currentClass );
+    if( (currentIndex+1) > (icons.length-1) ) {
+      elem.addClass( icons[0] );
+    }
+    else {
+      elem.addClass( icons[(currentIndex+1)] );
+    }
+  };
+
+  return privates;
+})(window, $( 'body' ), {}).attach();
